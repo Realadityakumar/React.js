c9216b4e127652d8b2a5bea9efb2760726636d02
Realadityakumar <adityakumar23214@lpu.in>
2024-06-19 11:01:10 +0530

    0.9v

--- Password_Generator/src/App.jsx
+++ Password_Generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect,useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
@@ -20,15 +20,22 @@ function App() {
     }
   setPassword(pass);
   },[length,numberAllowed,charAllowed,setPassword])
+
+  const passwordRef = useRef(null);
+
+  const copypasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password)
+  },[password])
   useEffect(() => {passwordGenerator();},[length,numberAllowed,charAllowed,passwordGenerator])
   return (
     <>
       <div className='w-full  max-w-md mx-auto shadow-md rounder-lg px-4 my-8 text-orange-500 bg-gray-700'>
         <h1 className='text-white text-center my-3'>Password generator</h1>
         <div className="flex shadow rounder-lg overflow-hidden pb-4">
-          <input type="" name="" id="" value={password} className='
+          <input type="" name="" id="" value={password} ref={passwordRef} className='
           outline-none w-full py-1 px-3 ' placeholder='Password' readOnly/>
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'  onClick={copypasswordToClipboard}>copy</button>
         </div>
         <div className="flex text-sm gap-x-2">
           <input type="range" min={6} max={100} value={length} onChange={(e) => {setLength(e.target.value)}} />
@@ -38,7 +45,7 @@ function App() {
           }}/>
           <label >Numbers</label>
           <input type="checkbox" defaultChecked={charAllowed} name="" id="characterInput" onChange={() => {
-            setNumberAllowed((prev) => !prev);
+            setCharAllowed((prev) => !prev);
           }}/>
           <label >Characters</label>
         </div>

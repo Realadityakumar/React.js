9ca831ce79fdaaf09e46af8b65b691b20d7c4657
Realadityakumar <adityakumar23214@lpu.in>
2024-07-09 10:45:42 +0530

    in debugging process

--- megaBlog/blog/src/components/index.js
+++ megaBlog/blog/src/components/index.js
@@ -1,6 +1,30 @@
+import Select from "./Select";
 import Header from "./header/header";
 import Footer from "./footer/footer";
-import Container from "./Container/container"
+import Container from "./Container/container";
 import Logo from "./logo";
 import LogoutBtn from "./header/LogoutBtn";
-export {Header,Footer,Container,Logo, LogoutBtn }
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}

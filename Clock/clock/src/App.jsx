b8391d2d1eb781d73bebfd9bf1c4512f1fc258d1
realadityakumar <adityakumar23214@lpu.in>
2025-03-16 16:42:36 +0530

    add start and stop functionality to clock using useRef for timer persistence

--- Clock/clock/src/App.jsx
+++ Clock/clock/src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
+import { useState,useRef } from 'react'
 import './App.css'
 
 function App() {
   
   const [currentClock,setCurrentClock] = useState(0);
-
+  const timer = useRef(0); // we use useref just to presist re render 
   function startClock(){
-    setInterval(() => {
-      setCurrentClock(c => c+1);
+    let value = setInterval(() => {
+     setCurrentClock(c => c+1);
     }, 1000);
+    timer.current = value;
+  }
+  function stopClock(){
+    clearInterval(timer.current);
   }
   return (
     <>
      {currentClock}
      <br />
      <button onClick={startClock}>start</button>
-   
+    <button onClick={stopClock}>stop</button>
     </>
   )
 }
